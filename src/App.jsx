91f6c3e2fecae86af62085e235870b6f48e16a2d
Kwenyerechukwu Paul <paulkwenye@gmail.com>
2024-05-27 13:27:57 +0100

    implement nested router and index router

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,23 @@ import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
+import CityList from "./components/CityList";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          {/* Index ROUTES */}
+          <Route index element={<CityList />} />
+          {/* Nexted Routes */}
+          <Route path="cities" element={<CityList />} />
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>form</p>} />
+        </Route>
         {/* if there is no one matching the router */}
         <Route path="*" element={<PageNotFound />} />
       </Routes>
---
+++ src/components/CityList.jsx
@@ -0,0 +1,6 @@
+import styles from "./CityList.module.css";
+function CityList() {
+  return <ul className={styles.cityList}>KIST</ul>;
+}
+
+export default CityList;
